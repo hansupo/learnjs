62031ed5b6ad2e1bd74cfb9c99b5f058aa9db05c
Hans Harald Pilv <hanspilv@gmail.com>
2025-03-06 13:07:17 +0200

    common vs es

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-import test from './test.js';
-test.test();
+// import es from './es.js';
+// es.test();
+
+
+const es = require('./es.js').default;
+es.test();
+
+// import common from './common.js';
+// common.hello();
+
+
+// const common = require('./common.js');
+// common.hello();
